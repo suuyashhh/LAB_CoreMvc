8594a6faa8b2baffaf7b2312d4b0ad82c5157303
suuyashhh <144991357+suuyashhh@users.noreply.github.com>
2026-01-18 19:06:13 +0530

    2 nd commit

---
+++ Lab_Mvc/Interfaces/DairyFarm/IBillDairy.cs
@@ -0,0 +1,13 @@
+﻿using Models.DairyFarm;
+
+namespace Lab_Mvc.Interfaces.DairyFarm
+{
+    public interface IBillDairy
+    {
+        Task<IEnumerable<DTOBillDairy>> GetAllBillHistory(int userId);
+        Task<object> GetBillImageById(long exp_id);
+        Task Save(DTOBillDairy objFeed);
+        Task Edit(DTOBillDairy objFeed);
+        Task Delete(long exp_id);
+    }
+}
---
+++ Models/DairyFarm/DTOBillDairy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DairyFarm
+{
+    public class DTOBillDairy
+    {
+        public int? bill_id { get; set; }
+        public int? user_id { get; set; }
+        public string? animal_type { get; set; }
+        public int? price { get; set; }
+        public DateTime? date { get; set; }
+        public string? BillImage { get; set; } // base64 image string
+    }
+}
