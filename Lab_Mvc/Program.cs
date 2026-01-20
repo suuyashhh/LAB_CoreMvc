eda9bc88ac1737d891db5d972f7df36bbfc991f2
suuyashhh <144991357+suuyashhh@users.noreply.github.com>
2026-01-20 20:27:51 +0530

    fsdfsdfxds

--- Lab_Mvc/Program.cs
+++ Lab_Mvc/Program.cs
@@ -45,6 +45,7 @@ builder.Services.AddScoped<IOtherFeeds, OtherFeedsRepository>();
 builder.Services.AddScoped < IBillDairy, BillDairyRepository>();
 builder.Services.AddScoped < IHistoryDairy, HistoryDairyRepository>();
 builder.Services.AddScoped <IAnimalHealthHistory, AnimalHealthHistoryRepository>();
+builder.Services.AddScoped <IBreedingDateCheck, BreedingDateCheckRepository>();
 
 builder.Services.AddSingleton<IConnectionMultiplexer>(
     ConnectionMultiplexer.Connect("localhost:6379,abortConnect=false")
